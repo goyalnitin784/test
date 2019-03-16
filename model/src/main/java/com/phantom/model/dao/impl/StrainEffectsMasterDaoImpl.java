377083cb108dfabb6e712cd95177441b53b393d0
nitin.goyal <nitin.goyal@yatra.com>
2019-03-16 23:08:01 +0530

    dao commit

---
+++ model/src/main/java/com/phantom/model/dao/impl/StrainEffectsMasterDaoImpl.java
@@ -0,0 +1,7 @@
+package com.phantom.model.dao.impl;
+
+import com.phantom.model.dao.StrainEffectsMasterDao;
+import com.phantom.model.entity.StrainEffectsMaster;
+
+public class StrainEffectsMasterDaoImpl extends GenericHibernateDAO<StrainEffectsMaster, Long> implements StrainEffectsMasterDao {
+}
---
+++ model/src/main/java/com/phantom/model/dao/impl/TwitterFeedsDaoImpl.java
@@ -0,0 +1,7 @@
+package com.phantom.model.dao.impl;
+
+import com.phantom.model.dao.TwitterFeedsDao;
+import com.phantom.model.entity.TwitterFeeds;
+
+public class TwitterFeedsDaoImpl extends GenericHibernateDAO<TwitterFeeds, Long> implements TwitterFeedsDao {
+}
